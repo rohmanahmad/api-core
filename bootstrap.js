a895d6834a01d27f30fb3d04bc2f393e9a8812f3
rohmanahmad <rohmanmail@gmail.com>
2020-09-21 22:29:47 +0700

    remove all service command

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,13 @@
 const {readFileSync} = require('fs')
 const path = require('path')
+const fplugin = require('fastify-plugin')
+const moment = require('moment-timezone')
+const helmet = require('fastify-helmet')
+const fastifyEnv = require('fastify-env')
+const formbody = require('fastify-formbody')
+const fastify = require('fastify')
+const postgres = require('fastify-postgres')
+const fastifyRedis = require('fastify-redis')
 
 const controllers = {
     AuthenticationController: require('./modules/v1.0/controllers/AuthenticationController'),
@@ -78,21 +86,13 @@ const configurations = {
     SwaggerForPartners: require('./modules/v1.0/configurations/swagger/Partners')
 }
 
-const commands = {
-    EmailSender: require('./modules/v1.0/commands/EmailSender'),
-    WhatsappSender: require('./modules/v1.0/commands/WhatsappSender'),
-    Migration: require('./modules/v1.0/commands/Migration'),
-    DummyData: require('./modules/v1.0/commands/DummyData'),
-    Test: require('./modules/v1.0/commands/Test')
-}
-
 const statics = {
     GoogleLogin: readFileSync(path.join(__dirname, 'modules/v1.0/statics/GoogleLogin.html'), {encoding: 'utf-8'})
 }
 
 const environments = process.env
 
-const m = {models, controllers, routes, services, middlewares, helpers, configurations, statics, environments, commands}
+const m = {models, controllers, routes, services, middlewares, helpers, configurations, statics, environments}
 const use = function (mode, name) {
     /* 
     example:
@@ -112,10 +112,151 @@ const use = function (mode, name) {
        process.exit(0)
    }
 }
-
-const plugin = require('fastify-plugin')
-exports.use = use
-exports.plugin = plugin(function (instance, opts, next) {
+const plugin = fplugin(function (instance, opts, next) {
     instance.decorate('include', use)
     next()
 })
+
+const server = function () {
+    const instance = fastify({
+        logger: {
+            level: 'info',
+            serializers: {
+                user (req) {
+                    return {
+                        method: req.method,
+                        url: req.url,
+                        headers: req.headers,
+                        hostname: req.hostname,
+                        remoteAddress: req.ip,
+                        remotePort: req.connection.remotePort
+                    }
+                }
+            }
+        },
+        // connectionTimeout: 1000 * 60 * 10,
+        ignoreTrailingSlash: true, // registers both "/foo" and "/foo/" :: tanpa / dibelakang atau tidak, sama saja
+        caseSensitive: true // https://www.fastify.io/docs/latest/Server/#casesensitive
+    })
+
+    // register form parser (application/x-www-form-urlencoded)
+    instance.register(formbody)
+
+    const route = require('./modules/v1.0/routes')
+
+    // fastify.register(plugin)
+    instance.decorate('include', use)
+
+    // monitoring request url
+    instance.addHook('onRequest', (req, reply, done) => {
+        console.info(`[${req.method}]`, req.url)
+        done()
+    })
+
+    // redis driver: https://github.com/fastify/fastify-redis
+    instance
+        .register(fastifyRedis, {
+            url: process.env.REDIS_URI
+        })
+
+    /* Helmet : https://github.com/fastify/fastify-helmet */
+    instance.register(helmet, {
+        hidePoweredBy: { setTo: 'XHTML-5.10' }
+    })
+
+    /* read the env file :  */
+    instance.register(fastifyEnv, {
+        dotenv: {
+            path: `${__dirname}/.env`,
+            debug: true
+        },
+        schema: {
+            type: 'object',
+            required: [ 'APP_PORT' ],
+            properties: {
+            PORT: {
+                type: 'string',
+                default: 3000
+            }
+            }
+        }
+    })
+
+    /* db postgresql : https://github.com/fastify/fastify-postgres */
+    instance.register(postgres, function () {
+        return {
+            connectionString: process.env.POSTGRESQL_DSN
+        }
+    })
+
+    /* documentation : https://github.com/fastify/fastify-swagger*/
+    instance.register(require('./customize/fastify-swagger'), use('configurations', 'SwaggerForClient'))
+    instance.register(require('./customize/fastify-swagger'), use('configurations', 'SwaggerForPartners')) // using custom 
+    instance.register(require('./customize/fastify-swagger'), use('configurations', 'SwaggerForAdmin')) // using custom 
+
+    /* registering routes */
+    // register route harus dibawah nya swagger, karena swagger membaca schema yg di dapat dari masing2 routes yg didaftarkan
+    instance.register(route(instance, 'SwaggerForClient'))
+    instance.register(route(instance, 'SwaggerForPartners'))
+    instance.register(route(instance, 'SwaggerForAdmin'))
+
+    /* set notfound response handler */
+    instance.setNotFoundHandler({}, function (request, response) {
+        response.send({
+            message: `(${request.url}) Route not found`,
+            error: 'You need add custom headers like accept-version and another header',
+            statusCode: 404,
+            // errors: err // gak ada
+        })
+    })
+
+    /* set error response handler */
+    instance.setErrorHandler(function (err, request, response) {
+        console.error(err)
+        response
+            .status(500)
+            .send({
+                statusCode: 500,
+                message: err.message,
+                processId: request.id,
+                errors: err.stack.toString()
+            })
+    })
+
+    console.logger = function (...args) {
+        console.log(`[${moment().tz('Asia/Jakarta').format('LLL')}]`, ...args)
+    }
+    /* trying to open port */
+    instance.ready(function (err) {
+        try {
+            if (err) {
+                console.error(err)
+                process.exit(0)
+            }
+            instance.swaggerforclient({zoneType: 'client', host: process.env.APP_DOMAIN})
+            instance.swaggerforpartners({zoneType: 'partners', host: process.env.APP_DOMAIN})
+            instance.swaggerforadmin({zoneType: 'admin', host: process.env.APP_DOMAIN})
+            instance.listen(process.env.APP_PORT, process.env.APP_HOST)
+            .then(async (address) => {
+                const ActivityService = instance.include('services', 'TaskService')(instance)
+                ActivityService
+                    .createRestartActivity()
+                    .then(function () {
+                        console.log(`server listening on ${address}`)
+                    })
+            })
+            .catch((err) => {
+                console.error(err)
+                process.exit(0)
+            })
+        } catch (err2) {
+            console.error(err2)
+        }
+    })
+}
+
+module.exports = {
+    use,
+    plugin,
+    server
+}
