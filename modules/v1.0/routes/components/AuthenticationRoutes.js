320317254d8f0484b26d76cf0a6b70654b32449c
rohmanahmad <rohmanmail@gmail.com>
2020-10-03 00:59:37 +0700

    update authentication

--- modules/v1.0/routes/components/AuthenticationRoutes.js
+++ modules/v1.0/routes/components/AuthenticationRoutes.js
@@ -18,13 +18,48 @@ const routes = [
                 200: {
                     type: 'object',
                     properties: {
-                        statusCode: {type: 'number', example: 200},
-                        message: {type: 'string', example: 'ok'},
-                        data: {type: 'object', properties: {
-                            messageText: { type: 'string' },
-                            status: { type: 'string' },
-                            otp_dev: { type: 'string' }
-                        }}
+                        statusCode: { type: 'number', example: 200 },
+                        message: { type: 'string', example: 'ok' },
+                        data: {
+                            type: 'object',
+                            properties: {
+                                item: {
+                                    type: 'object',
+                                    properties: {
+                                        messageText: { type: 'string' },
+                                        status: { type: 'string' }
+                                    }
+                                },
+                                next_stage: {
+                                    type: 'object',
+                                    properties: {
+                                        name: {type: 'string', example: 'VALIDATE_OTP'},
+                                        ajax: {
+                                            type: 'object',
+                                            properties: {
+                                                method: { type: 'string', example: 'GET' },
+                                                url: { type: 'string', example: '/api/v1/client/validate/otp' },
+                                                querystring: {
+                                                    type: 'object',
+                                                    example: {query1: '@value'}
+                                                },
+                                                formdata: {
+                                                    type: 'object',
+                                                    example: {input1: '@value'}
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+                                meta: {
+                                    type: 'object',
+                                    example: {
+                                        meta1: '@value',
+                                        otp_dev: '10203040'
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -52,11 +87,43 @@ const routes = [
                         data: {
                             type: 'object',
                             properties: {
-                                type: { type: 'string' },
-                                need_update_profile: { type: 'boolean' },
-                                go_to: { type: 'string', example: '/dashboard > available if need_update_profile = false' },
-                                token: { type: 'string', example: 'hash with >30 char'},
-                                valid_until: { type: 'string', example: '2020-02-01 23:00:21'}
+                                item: {
+                                    type: 'object',
+                                    properties: {
+                                        type: { type: 'string' },
+                                        need_update_profile: { type: 'boolean' },
+                                        go_to: { type: 'string', example: '/dashboard > available if need_update_profile = false' },
+                                        token: { type: 'string', example: 'hash with >30 char'},
+                                        valid_until: { type: 'string', example: '2020-02-01 23:00:21'}
+                                    }
+                                },
+                                next_stage: {
+                                    type: 'object',
+                                    properties: {
+                                        name: {type: 'string', example: 'PROFILE_UPDATE'},
+                                        ajax: {
+                                            type: 'object',
+                                            properties: {
+                                                method: {type: 'string', example: 'GET'},
+                                                url: {type: 'string', example: '/api/v1/client/profile/update'},
+                                                querystring: {
+                                                    type: 'object',
+                                                    example: {query1: '@value'}
+                                                },
+                                                formdata: {
+                                                    type: 'object',
+                                                    example: {input1: '@value'}
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+                                meta: {
+                                    type: 'object',
+                                    example: {
+                                        meta1: '@value'
+                                    }
+                                }
                             }
                         }
                     }
